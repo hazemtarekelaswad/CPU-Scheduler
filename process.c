52a562ae92a0e4ce470cbd681ff46b9ec4f17a48
= <=>
2021-12-11 01:18:56 +0200

    Fix process.c

--- process.c
+++ process.c
@@ -1,7 +1,8 @@
 #include "headers.h"
 
 /* Modify this file as needed*/
-int remainingtime;
+int remainingTime;
+int runningTime;
 int finishTime;
 int msgQueueProcessID;
 
@@ -12,37 +13,42 @@ struct TerminationMsg {
 
 int main(int agrc, char * argv[])
 {
+    // printf("PROCESS CREATED with PID: %d\n", getpid());
     initClk();
     int startTime = getClk();
 
-    remainingtime = atoi(argv[1]);  // Initially, the remaining time = running time
-    while (remainingtime > 0)
+    runningTime = atoi(argv[1]);  // Initially, the remaining time = running time
+    remainingTime = runningTime;
+    // printf("Running Time: %d\n", remainingTime);
+    while (remainingTime > 0)
     {
-        remainingtime -= getClk() - startTime;
+        remainingTime = runningTime - (getClk() - startTime);
         
-        if (remainingtime == 0)
+        if (remainingTime == 0) {
             finishTime = getClk();
+        }
     }
+    // printf("CLK: %d     PROCESS FINISHED with Finish Time: %d\n", getClk(), finishTime);
 
-    struct TerminationMsg termMsg = { 5, finishTime };
+    // struct TerminationMsg termMsg = { 5, finishTime };
     
-    system("touch Keys/process_scheduler_msgQ");
-    int fileKey = ftok("Keys/process_scheduler_msgQ", 'B');
+    // system("touch Keys/process_scheduler_msgQ");
+    // int fileKey = ftok("Keys/process_scheduler_msgQ", 'B');
 
-    msgQueueProcessID = msgget(fileKey, 0666 | IPC_CREAT);
-    if (msgQueueProcessID == -1) {
-        perror("ERROR occured during creating the message queue in the process file\n");
-        exit(-1);
-    }
+    // msgQueueProcessID = msgget(fileKey, 0666 | IPC_CREAT);
+    // if (msgQueueProcessID == -1) {
+    //     perror("ERROR occured during creating the message queue in the process file\n");
+    //     exit(-1);
+    // }
 
-    // 2. Send the finish time to the scheduler
-    int isSent = msgsnd(msgQueueProcessID, &termMsg, sizeof(termMsg) - sizeof(termMsg.type), !IPC_NOWAIT);
-    if (isSent == -1) {
-        perror("ERROR occured during sending the finish time to the scheduler\n");
-        exit(-1);
-    }
+    // // 2. Send the finish time to the scheduler
+    // int isSent = msgsnd(msgQueueProcessID, &termMsg, sizeof(termMsg) - sizeof(termMsg.type), !IPC_NOWAIT);
+    // if (isSent == -1) {
+    //     perror("ERROR occured during sending the finish time to the scheduler\n");
+    //     exit(-1);
+    // }
 
 
     destroyClk(false);
-    return 0;
+    exit(finishTime);
 }
