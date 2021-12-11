bc888f1f69d0792c0c6e7880e060681e880faa41
Hazem Elaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2021-12-11 13:07:50 +0200

    Use the system clock in process.c instead of simulated clock

--- process.c
+++ process.c
@@ -1,54 +1,25 @@
 #include "headers.h"
+#include <time.h>
 
-/* Modify this file as needed*/
-int remainingTime;
-int runningTime;
-int finishTime;
-int msgQueueProcessID;
-
-struct TerminationMsg {
-    long type;
-    int finishTime;
-};
 
 int main(int agrc, char * argv[])
 {
-    // printf("PROCESS CREATED with PID: %d\n", getpid());
     initClk();
-    int startTime = getClk();
+    clock_t startTime = clock();
 
-    runningTime = atoi(argv[1]);  // Initially, the remaining time = running time
-    remainingTime = runningTime;
-    // printf("Running Time: %d\n", remainingTime);
+    clock_t runningTime = (clock_t)atoi(argv[1]);  
+    clock_t remainingTime = runningTime;    // Initially, the remaining time = running time
+
     while (remainingTime > 0)
     {
-        remainingTime = runningTime - (getClk() - startTime);
-        
-        if (remainingTime == 0) {
-            finishTime = getClk();
-        }
+        remainingTime = runningTime * CLOCKS_PER_SEC - (clock() - startTime);
     }
-    // printf("CLK: %d     PROCESS FINISHED with Finish Time: %d\n", getClk(), finishTime);
-
-    // struct TerminationMsg termMsg = { 5, finishTime };
+    int simFinishTime = getClk();
     
-    // system("touch Keys/process_scheduler_msgQ");
-    // int fileKey = ftok("Keys/process_scheduler_msgQ", 'B');
-
-    // msgQueueProcessID = msgget(fileKey, 0666 | IPC_CREAT);
-    // if (msgQueueProcessID == -1) {
-    //     perror("ERROR occured during creating the message queue in the process file\n");
-    //     exit(-1);
-    // }
-
-    // // 2. Send the finish time to the scheduler
-    // int isSent = msgsnd(msgQueueProcessID, &termMsg, sizeof(termMsg) - sizeof(termMsg.type), !IPC_NOWAIT);
-    // if (isSent == -1) {
-    //     perror("ERROR occured during sending the finish time to the scheduler\n");
-    //     exit(-1);
-    // }
-
+    clock_t finishTime = clock();
+    // printf("Duration: %ld\n", (finishTime - startTime) / CLOCKS_PER_SEC);
+    // printf("FinishTime: %ld\n", finishTime / CLOCKS_PER_SEC);
 
     destroyClk(false);
-    exit(finishTime);
+    exit(simFinishTime);
 }
