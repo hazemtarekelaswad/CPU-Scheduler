ca354c2fe56d262baea0b57d446270e1b075530f
husseinAhmed10 <85680681+husseinAhmed10@users.noreply.github.com>
2021-12-17 17:36:05 +0200

    Update Outputfile_functions.h
    Adding parameters to the function

--- Outputfile_functions.h
+++ Outputfile_functions.h
@@ -7,42 +7,44 @@ fflush(f);
 fclose(f);
 }
 //When Starting a process
-void OutFile_Starting (file* f)
+void OutFile_Starting (file* f, int id_curProc, int AT_curProc, int RunT_curProc, int RemT_curProc, int WaitT_curProc)
 {
 f = fopen("logs/scheduler.log", "a");
 fprintf(f, "At time\t%d\tprocess\t%d\tstarted\tarr\t%d\ttotal\t%d\tremain\t%d\twait\t%d\n",
-getClk(),cur_process->id
-,cur_process->arrivalTime,cur_process->runTime
-,cur_process->remainingTime,cur_process->wait_time);
+getClk(),id_curProc
+,AT_curProc,RunT_curProc
+,RemT_curProc,WaitT_curProc);
 fflush(f);
 fclose(f);
 }
 //When stopping a process
-void OutFile_Stopping (file* f)
+void OutFile_Stopping (file* f, int id_prevProc, int AT_prevProc, int RunT_prevProc, int RemT_prevProc, int WaitT_prevProc)
 {
 	f = fopen("logs/Round_Robin/scheduler2.log", "a");
-     fprintf(f, "At time\t%d\tprocess\t%d\tstopped\tarr\t%d\ttotal\t%d\tremain\t%d\twait\t%d\n",getClk(),prev_process->id
-     ,prev_process->arrivalTime,prev_process->runTime,prev_process->remainingTime,prev_process->wait_time);
+     fprintf(f, "At time\t%d\tprocess\t%d\tstopped\tarr\t%d\ttotal\t%d\tremain\t%d\twait\t%d\n",
+	 getClk(), id_prevProc
+     ,AT_prevProc,RunT_prevProc,RemT_prevProc,WaitT_prevProc);
      // fflush(fp);
      fclose(f);
 }
 //When resuming a process
-void OutFile_Resuming (file* f)
+void OutFile_Resuming (file* f , int id_curProc, int AT_curProc, int RunT_curProc, int RemT_curProc, int WaitT_curProc)
 {
 f = fopen("logs/scheduler.log", "a");
-fprintf(f, "At time\t%d\tprocess\t%d\tresumed\tarr\t%d\ttotal\t%d\tremain\t%d\twait\t%d\n",getClk(),cur_process->id
-       ,cur_process->arrivalTime,cur_process->runTime,cur_process->remainingTime,cur_process->wait_time);
+fprintf(f, "At time\t%d\tprocess\t%d\tresumed\tarr\t%d\ttotal\t%d\tremain\t%d\twait\t%d\n",
+		getClk(),id_curProc
+       ,AT_curProc,RunT_curProc,RemT_curProc,WaitT_curProc);
        // fflush(fp);
        fclose(f)
 }
 //When finishing a process
-void OutFile_Finishing (file* f)
+void OutFile_Finishing (file* f, , int id_curProc, int AT_curProc, int RunT_curProc, int RemT_curProc, int WaitT_curProc)
 {
 f = fopen("logs/scheduler.log", "a");
 fprintf(f, "At time\t%d\tprocess\t%d\tfinished\tarr\t%d\ttotal\t%d\tremain\t%d\twait\t%d\tTA\t%d\tWTA\t%.2f\n",
-                getClk(),cur_process->id
-                ,cur_process->arrivalTime,cur_process->runTime,cur_process->remainingTime,
-                cur_process->wait_time,getClk()-(cur_process->arrivalTime),WTA);
+                getClk(),id_curProc
+                ,AT_curProc,RunT_curProc,RemT_curProc,
+                WaitT_curProc,getClk()-AT_curProc,(getClk()-AT_curProc)/(RunT_curProc));
                     fclose(f);
 }
 
